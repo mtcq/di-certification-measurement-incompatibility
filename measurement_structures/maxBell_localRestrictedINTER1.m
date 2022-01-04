91fb7bc101cf44d1beaccf3a5ff51cd4c891e074
Marco Túlio Quintino <mtcq.mm@gmail.com>
2022-01-04 14:09:51 +0100

    Update maxBell_localRestrictedINTER1.m

--- measurement_structures/maxBell_localRestrictedINTER1.m
+++ measurement_structures/maxBell_localRestrictedINTER1.m
@@ -12,7 +12,7 @@ Ia=dS(3); %Number of inputs for Alice
 Ib=dS(4); %Number of inputs for Bob
 
 cvx_begin SDP quiet  %start SDP
-cvx_solver SEDUMI
+%cvx_solver SEDUMI
 %cvx_precision best
 
     variable p(Oa,Ob,Ia,Ib) %Declare the SDP variable for the distribution p
@@ -30,9 +30,9 @@ for a=1:Oa
     end
 end
 
-isLOC_restricted(p,[1 2]);  %Impose the constraint that  p should be in L^{Q_level}_{12}
-isLOC_restricted(p,[2 3]);  %Impose the constraint that  p should be in L^{Q_level}_{23}
-isLOC_restricted(p,[1 3]);  %Impose the constraint that  p should be in L^{Q_level}_{13}
+isLOC_restricted1(p,[1 2]);  %Impose the constraint that  p should be in L^{Q_level}_{12}
+isLOC_restricted1(p,[2 3]);  %Impose the constraint that  p should be in L^{Q_level}_{23}
+isLOC_restricted1(p,[1 3]);  %Impose the constraint that  p should be in L^{Q_level}_{13}
 NPAHierarchy1(p,level);     %Impose that the total distributiion respects some level of the NPA hieararchy
 
 
